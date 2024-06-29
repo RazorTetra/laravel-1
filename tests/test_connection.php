a9a4d8365a8739343e481209dec150bb2832f3c3
toniliu <toniliu672@gmail.com>
2024-06-30 01:51:51 +0800

    first commit

---
+++ tests/test_connection.php
@@ -0,0 +1,26 @@
+<?php
+
+// Load environment variables
+require __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_DATABASE');
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$conn->close();
+?>
